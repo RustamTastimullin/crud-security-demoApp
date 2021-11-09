31f5fdc05452d5ff56f1e4b14833793f48cafd42
Rustam Tastimullin <tastimullin@mail.ru>
2021-11-09 21:38:16 +0500

    Initial commit

---
+++ src/main/java/com/webapp/webApp/BestAppApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestAppApplication.class, args);
+	}
+
+}
